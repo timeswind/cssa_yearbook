33c898cc41b262f428fc7268b313fd304ab7e1bf
Mingtian Yang <tswymt@gmail.com>
2019-04-12 11:18:04 -0400

    update shell scripts

--- git-pull-all.sh
+++ git-pull-all.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 true > bookname.data
-git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote"; git worktree add "${remote#origin/}" "$remote"; echo "${remote#origin/}" >> bookname.data; done
+git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote"; git worktree add "${remote#origin/}" "$remote"; echo "${remote#origin/}" >> bookname.data; cp -R ./${remote#origin/}/.gitbook ./${remote#origin/}/gitbook; done
 # git fetch --all
 # git pull --all
